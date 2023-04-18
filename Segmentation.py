63dbd7649f9c73c249096890654b591a8c429734
Pavel Emeliyanenko <p.emeliyanenko@precitec-optronik.de>
2023-04-18 17:13:00 +0200

    deeplabv3+ model first version

--- Segmentation.py
+++ Segmentation.py
@@ -87,18 +87,22 @@ def loss_initializer(self):
 
         return loss
 
-# Atrous Spatial Pyramid Pooling (ASPP) Block
-def ASPP_block(inputs, baseModel, num_filters, dilation_rates, relu_fn='relu'):
+# using Atrous Spatial Pyramid Pooling (ASPP) Block and decoder architecture
+def ASPPBlock(features, num_filters, dilation_rates, relu_fn='relu'):
 
-    features = baseModel(inputs)
     X = features
+    # shape: (Batch, H, W, C)
     shape = X.shape # tuple of 4 params
     use_bias=False
+
+    # perhaps, the baseModel shall return outputs from intermediate layer too..
 
     # TODO: check this with non-square inputs !!
     #outMean = tfl.AveragePooling2D(pool_size=(shape[-3], shape[-2]))(X)  
     outMean = tfl.GlobalAveragePooling2D(keepdims=True)(X)
     outMean = ConvBlock(relu_fn, filters=num_filters, kernel_size=1, padding='same', use_bias=True)(outMean)
+
+    # first rows then columns
     newSz = (shape[-3]//outMean.shape[1], shape[-2]//outMean.shape[2])
     outMean = tfl.UpSampling2D(size=newSz, interpolation='bilinear')(outMean)
     #outMean = tf.reduce_mean(X, axis=[1,2], keepdims=True)
@@ -113,5 +117,46 @@ def ASPP_block(inputs, baseModel, num_filters, dilation_rates, relu_fn='relu'):
     X = tfl.Concatenate(axis=-1)(dilated)
     X = ConvBlock(relu_fn, filters=num_filters, kernel_size=1, padding='same', use_bias=use_bias)(X)
     #X = tfl.Activation()
+    return X
 
-    return tf.keras.Model(inputs=inputs, outputs=X)
+# downscale_factors - a list of downscale factors which defines the number of intermediate connections
+# E.g.: downscale_factors = [4, 4]: there will be two intermediate connections for layers with shape size 
+# input_shape/4, and input_shape/16 
+# the top layer (the one with largest downscale factor) is connected to ASPP block
+def DeepLabv3Plus(baseModel, downscale_factors, num_filters, dilation_rates, relu_fn='relu'):
+
+    layerIdxs = [] # save layer index and its associated scale factor (relative to the previous layer)
+    L = baseModel.layers
+    for s in downscale_factors:
+        # reference shape with scaling factor
+        idx = layerIdxs[-1][0] if layerIdxs else 0
+        #print(f"s={s}; idxs={layerIdxs}; idx={idx}")
+        shapeI = L[idx].output.shape
+        idx = -1
+        for i,layer in enumerate(L): 
+            shape = layer.output.shape
+            if shape[1]*s == shapeI[1] and shape[2]*s == shapeI[2]:
+                idx = i
+        layerIdxs.append((idx,s))
+
+    idx, s = layerIdxs[-1]
+    X = L[idx].output
+    X = ASPPBlock(X, num_filters, dilation_rates, relu_fn)
+    X = tfl.UpSampling2D(size=(s,s), interpolation='bilinear')(X)
+    #shape = X.shape
+    #X = tf.image.resize(X, size=[shape[1]*s,shape[2]*s], method=tf.image.ResizeMethod.BILINEAR)
+ 
+    # iterate skipping the last item 
+    for idx,s in reversed(layerIdxs[:-1]):
+        Z = L[idx].output
+        Z = ConvBlock(relu_fn, filters=num_filters, kernel_size=1, padding='same', use_bias=False)(Z)
+        X = tfl.Concatenate()([X, Z])
+        X = ConvBlock(relu_fn, filters=num_filters, kernel_size=3, padding='same', use_bias=False)(X)
+        X = ConvBlock(relu_fn, filters=num_filters, kernel_size=3, padding='same', use_bias=False)(X)
+        shape = X.shape
+        X = tfl.UpSampling2D(size=(s,s), interpolation='bilinear')(X)
+        #X = tf.image.resize(X, size=[shape[1]*scaleL,shape[2]*scaleL], method=tf.image.ResizeMethod.BILINEAR)
+
+    out = tfl.Conv2D(2, kernel_size=1, padding='same')(X)
+
+    return tf.keras.Model(inputs=baseModel.input, outputs=out)
